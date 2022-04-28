54ad4cf634d980281755b6db52ceaa377f0251ff
Suanya <sonjaunger@eyewant.me>
2022-04-28 09:20:33 -0700

    resetting countdown

---
+++ CS-Jam1/Assets/Scripts/Sonja/CountDownTimer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public int level;
+    public GameManager gameManager;
+
+    public float timer = 3f;
+    [SerializeField] private TextMeshProUGUI timerSeconds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene(level);
+        }
+
+    }
+}
